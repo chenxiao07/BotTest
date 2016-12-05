e8e3b80f73f75da9892072f065e2561264c579eb
CHEN XIAO <xiao.chen@linecorp.com>
2016-12-05 19:00:08 +0900

    test

--- linebot.js
+++ linebot.js
@@ -48,15 +48,4 @@ app.post('/callback', function(req, res){
   });
 });
 
-var sslOptions = {
-  key: fs.readFileSync('ssl-cert/server.key'),
-  cert: fs.readFileSync('ssl-cert/server.crt'),
-  ca: fs.readFileSync('ssl-cert/ca.crt'),
-  requestCert: true,
-  rejectUnauthorized: false,
-  passphrase: 'sxhctycgr-'
-};
-
-var secureServer = https.createServer(sslOptions, app).listen('3030', function () {
-  console.log("Secure server listening on port 3030");
-});
+app.listen(3030);
